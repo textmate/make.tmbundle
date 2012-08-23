bf23c1c0bee89576444b055c9b190c4cb5da0824
Adam Lickel <adam@lickel.com>
2012-08-23 01:26:15 -0700

    Make: Encapsulate make commands into /lib/make.rb
    This lets us have consistent variables and regex handling.
    It also makes it very easy to add new commands (see: Build vs Run).

---
+++ Support/lib/make.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+require ENV["TM_SUPPORT_PATH"] + "/lib/tm/executor"
+require ENV["TM_SUPPORT_PATH"] + "/lib/tm/save_current_document"
+require ENV["TM_SUPPORT_PATH"] + "/lib/escape"
+
+TM_MAKE = e_sh(ENV['TM_MAKE'] || 'make')
+
+TextMate::Executor.make_project_master_current_document
+
+# Find our project directory
+ENV["TM_PROJECT_DIRECTORY"] ||= File.dirname(ENV["TM_FILEPATH"])
+
+# Find and verify our makefile
+Dir.chdir(ENV["TM_PROJECT_DIRECTORY"])
+ENV["TM_MAKE_FILE"] = ENV["TM_PROJECT_DIRECTORY"] + "/Makefile" if ENV["TM_MAKE_FILE"].nil? or not File.file?(ENV["TM_MAKE_FILE"])
+
+# Go next to the makefile
+Dir.chdir(File.dirname(ENV["TM_MAKE_FILE"]))
+TM_MAKE_FILE = File.basename(ENV["TM_MAKE_FILE"])
+
+TM_MAKE_FLAGS = ["-w"]
+TM_MAKE_FLAGS << "-f" + TM_MAKE_FILE
+TM_MAKE_FLAGS << ENV["TM_MAKE_FLAGS"] unless ENV["TM_MAKE_FLAGS"].nil?
+
+def perform_make(target = nil)
+  ENV["TM_DISPLAYNAME"] = target || "default"
+
+  dirs = [ENV['TM_PROJECT_DIRECTORY']]
+  flags = TM_MAKE_FLAGS
+  flags << target unless target.nil?
+  TextMate::Executor.run(TM_MAKE, flags, :verb => "Making", :use_hashbang => false) do |line, type|
+    if line =~ /^g?make.*?: Entering directory `(.*?)'$/ and not $1.nil? and File.directory?($1)
+      dirs.unshift($1)
+      ""
+    elsif line =~ /^g?make.*?: Leaving directory `(.*?)'$/ and not $1.nil? and File.directory?($1)
+      dirs.delete($1)
+      ""
+    elsif line =~ /^(.*?):(?:(\d+):)?(?:(\d+):)?\s*(.*?)$/ and not $1.nil?
+      file, lineno, column, message = $1, $2, $3, $4
+      path = dirs.map{ |dir| File.expand_path(file, dir) }.find{ |path| File.file? path }
+      unless path.nil?
+        parms =  [    "url=file://#{e_url path}" ]
+        parms << [   "line=#{lineno}"            ] unless lineno.nil?
+        parms << [ "column=#{column}"            ] unless column.nil?
+        info = file
+        info << " at line #{lineno}" unless lineno.nil?
+        info << ", column #{column}" unless column.nil?
+        info << "."
+        info = info.gsub('&', '&amp;').gsub('<', '&lt;').gsub('"', '&quot;')
+        "<a href=\"txmt://open?#{parms.join '&'}\" title=\"#{info}\">#{htmlize message}</a><br>\n"
+      end
+    end
+  end
+end
